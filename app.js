4f12b7e3d2973f8a1cf3ab3ad90d7c6d00555152
StevieNG <sengfongng@gmail.com>
2018-11-22 10:49:49 +0000

    added a second ship

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 let x=canvas.width/2
@@ -12,89 +13,122 @@ shipImage.src="./img/ship.svg"
 
 // let shipImage= new Image()
 // shipImage.src="./img/ship.svg"
-    // ctx.drawImage(shipImage,0,0)
+   // ctx.drawImage(shipImage,0,0)
 
-
-
-
+/* The functions to start and stop the game */
 function main(){
-    deltaX=0;
-    status.innerHTML= " Score: ???? " 
- intervalID = setInterval( drawShip, 100) }
-
-
- function stopGame(){
-    status.innerHTML= " GAME OVER!" 
-    clearInterval(intervalID) 
-    deltaX=0;
+   deltaX=0;
+   status.innerHTML= " Score: ???? "
+   intervalID = setInterval( drawShip, 100)
 }
-   
+
+
+function stopGame(){
+   status.innerHTML= " GAME OVER!"
+   clearInterval(intervalID)
+   deltaX=0;
+}
+
+
+/* Drawing the Ship that go on the top of the canvas */
+
 function drawShip(){
-    ctx.clearRect(0, 0, 700, 500)
-    ctx.fillStyle="#FF0000";
-     ctx.drawImage(shipImage,x-25+deltaX,450,50,30)
-    
-    // ctx.fillRect();
+   ctx.clearRect(0, 0, 700, 500)
+   ctx.fillStyle="#FF0000";
 
-    drawAlien();
+   if (deltaX >350){
+       deltaX=350;
+   }
 
-    drawBullet();
-
-    
+   else if(deltaX <-350){
+       deltaX=-350;
+   }
+   ctx.drawImage(shipImage,x-25+deltaX,450,50,30)
+   // ctx.fillRect();
+   drawAlien();
 
 
 }
 
 
+
+var ship = {
+    x:canvas.width,
+    y:canvas.height,
+    color:"black",
+    draw: function() {
+        ctx.fillStyle="black";
+        ctx.drawImage(shipImage,x-25+deltaX,450,50,30)
+    }
+
+
+}
+
+function clear() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+}    
+
+function draw(){
+    clear();
+    ship.draw()
+
+}
+
+/* Drawing the Aliens that go on the top of the canvas */
 
 function drawAlien(){
 
-    for(let i = 0; i < 12 ; i++) {
-        ctx.fillStyle="green";
-        ctx.fillRect(50+(i*50), 50, 30, 20); 
-        
-        ctx.fillStyle="lime";
-        ctx.fillRect(50+(i*50), 100, 30, 20);
-        
-        ctx.fillStyle="yellow";
-        ctx.fillRect(50+(i*50), 150, 30, 20);
+   for(let i = 0; i < 12 ; i++) {
+       ctx.fillStyle="green";
+       ctx.fillRect(50+(i*50), 50, 30, 20);
 
-        ctx.fillStyle="orange";
-        ctx.fillRect(50+(i*50), 200, 30, 20); 
-    }
+       ctx.fillStyle="lime";
+       ctx.fillRect(50+(i*50), 100, 30, 20);
+
+       ctx.fillStyle="yellow";
+       ctx.fillRect(50+(i*50), 150, 30, 20);
+
+       ctx.fillStyle="orange";
+       ctx.fillRect(50+(i*50), 200, 30, 20);
+   }
 }
 
+
+/* Drawing the Bullet that go on the top of the canvas */
+
 function drawBullet() {
-    for( let i = 0; i < 1; i++) {
-        ctx.beginPath()
+ for( let i = 0; i < 15  ; i++) {
+       
+    bulletInterval =setInterval( function(){ctx.beginPath()
         ctx.fillStyle = "black";
         ctx.arc(350, 460 - (i * 40), 5, 0, 2 * Math.PI, true)
         ctx.stroke();
-        ctx.fill();
+        ctx.fill()}, 1000)
+        
     }
 }
 
 
+/* The event listeners that move the ship */
 
-document.onkeydown = function(e){
-    if (e.keyCode === 37){
-        deltaX-=5;
-        console.log(deltaX)
-    }
-    else if (e.keyCode === 39){
-        deltaX+=5;
-        console.log(deltaX)
+document.onkeydown = function(e) {
 
-          
-    }
-    else if(e.keyCode === 32) {
-        drawBullet()
-    }
-}   
+   if (e.keyCode === 37){
+       deltaX-=5;
+       console.log(deltaX)
+   }
+   else if (e.keyCode === 39){
+       deltaX+=5;
+       console.log(deltaX)
+   } else if(e.keyCode === 32) {
+       console.log("true")
+       drawBullet()
+   }
+}
 
 
-    start.addEventListener("click", main)
-    stop.addEventListener("click", stopGame) 
-   
 
+main()
+//    start.addEventListener("click", main)
+//    stop.addEventListener("click", stopGame)
 
